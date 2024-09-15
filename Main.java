08a8693f473af8a7646d547eab17fe9fed381860
Ivan <geraldwallace3@mail.ru>
2024-09-15 13:30:38 +0300

    Lesson_7_5

--- Main.java
+++ Main.java
@@ -1,24 +1,21 @@
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.Scanner;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class Main {
+    public static void method5() {
+        Scanner scr = new Scanner(System.in);
+        int a = scr.nextInt();
+        int b = scr.nextInt();
+        int sum = a+b;
+        boolean b1 = true;
+        boolean b2 = false;
+        if ((sum>=10)&&(sum<=20)) {
+            System.out.println(b1);
+        } else System.out.println(b2);
+
+    }
+
     public static void main(String[] args) {
-        String[] words = {"Yellow", "Black", "Green", "Red", "Green", "Blue", "Yellow", "Red", "Red", "Pink", "Yellow"};
-        Set<String> uniqueWords = new HashSet<>();
-        Map<String, Integer> wordCount = new HashMap<>();
-        for (String word : words) {
-            uniqueWords.add(word);
-        }
-        for (String word : words) {
-            if (wordCount.containsKey(word)) {
-                wordCount.put(word, wordCount.get(word) + 1);
-            } else {
-                wordCount.put(word,1);
-            }
-        }
-        System.out.println("Уникальные слова: " + uniqueWords);
-        System.out.println("Количество повторений: " + wordCount);
+        Main.method5();
     }
 }
