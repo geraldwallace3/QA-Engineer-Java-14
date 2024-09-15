50d6b5c9517e3820d4a6f431a3ccf4b9be991faa
Ivan <geraldwallace3@mail.ru>
2024-09-15 13:42:37 +0300

    Lesson_11_2

--- Main.java
+++ Main.java
@@ -1,24 +1,15 @@
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
-
 public class Main {
     public static void main(String[] args) {
-        String[] words = {"Yellow", "Black", "Green", "Red", "Green", "Blue", "Yellow", "Red", "Red", "Pink", "Yellow"};
-        Set<String> uniqueWords = new HashSet<>();
-        Map<String, Integer> wordCount = new HashMap<>();
-        for (String word : words) {
-            uniqueWords.add(word);
-        }
-        for (String word : words) {
-            if (wordCount.containsKey(word)) {
-                wordCount.put(word, wordCount.get(word) + 1);
-            } else {
-                wordCount.put(word,1);
-            }
-        }
-        System.out.println("Уникальные слова: " + uniqueWords);
-        System.out.println("Количество повторений: " + wordCount);
+    Phonebook phonebook = new Phonebook();
+    phonebook.add("Первый", "1111");
+    phonebook.add("Второй", "2222");
+    phonebook.add("Третий", "3333");
+    phonebook.add("Четвертый", "4444");
+    phonebook.add("Первый", "5555");
+    phonebook.add("Второй", "6666");
+
+        System.out.println(phonebook.get("Первый"));
+        System.out.println(phonebook.get("Второй"));
+        System.out.println(phonebook.get("Четвертый"));
     }
 }
